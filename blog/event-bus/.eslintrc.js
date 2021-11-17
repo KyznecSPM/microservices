dacf808697993ed3e543a7003c581b3c19d070cc
Емельянов Дмитрий Олегович <dmitry_emelianov@epam.com>
2021-11-17 12:27:58 +0300

    feat: event bus handlers

--- blog/event-bus/.eslintrc.js
+++ blog/event-bus/.eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true,
+    es2021: true
   },
   extends: ['standard', 'prettier'],
   plugins: ['prettier'],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 12
   },
   rules: {
     'prettier/prettier': [
       'error',
       {
-        singleQuote: true,
-      },
-    ],
-  },
+        singleQuote: true
+      }
+    ]
+  }
 };
